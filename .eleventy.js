2dcdd41adadab7b29a103c18895705ccf4262932
Hugo Nogueira <hnogueira@marabyte.com>
2020-02-04 15:54:12 +0000

    minify HTML

--- .eleventy.js
+++ .eleventy.js
@@ -1,17 +1,21 @@
-const sassBuild = require('./lib/sass.11ty');
-const tailwindBuild = require('./lib/tailwind.11ty');
+const sassBuild = require("./lib/sass.11ty");
+const tailwindBuild = require("./lib/tailwind.11ty");
+const htmlmin = require("./lib/html-minify.11ty");
 
 module.exports = function(eleventyConfig) {
   // Input directory: src
   // Output directory: _site
 
   // Add filters to Nunjucks
-  eleventyConfig.addFilter('date', require('./site/_filters/date.11ty'));
-  eleventyConfig.addFilter('sass', sassBuild);
-  eleventyConfig.addNunjucksAsyncFilter('tailwind', tailwindBuild);
+  eleventyConfig.addFilter("date", require("./site/_filters/date.11ty"));
+  eleventyConfig.addFilter("sass", sassBuild);
+  eleventyConfig.addNunjucksAsyncFilter("tailwind", tailwindBuild);
 
   // The following copies to `_site/assets`
-  eleventyConfig.addPassthroughCopy('site/assets');
-  eleventyConfig.addPassthroughCopy('site/uploads');
-  eleventyConfig.addPassthroughCopy('admin');
+  eleventyConfig.addPassthroughCopy("site/assets");
+  eleventyConfig.addPassthroughCopy("site/uploads");
+  eleventyConfig.addPassthroughCopy("admin");
+
+  // Transforms
+  eleventyConfig.addTransform("htmlmin", htmlmin);
 };
