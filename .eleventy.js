9febbc42e251665bc1c5f8c93d30e67ffafc0213
Hugo Nogueira <hnogueira@marabyte.com>
2020-04-06 20:53:30 +0100

    update deps

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 const sassBuild = require("./lib/sass.11ty");
 const tailwindBuild = require("./lib/tailwind.11ty");
 const htmlmin = require("./lib/html-minify.11ty");
+const dateFormat = require("./lib/date.11ty");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 
 module.exports = function(eleventyConfig) {
@@ -8,14 +9,13 @@ module.exports = function(eleventyConfig) {
   // Output directory: _site
 
   // Add filters to Nunjucks
-  eleventyConfig.addFilter("date", require("./lib/date.11ty"));
+  eleventyConfig.addFilter("date", dateFormat);
   eleventyConfig.addFilter("sass", sassBuild);
   eleventyConfig.addNunjucksAsyncFilter("tailwind", tailwindBuild);
 
   // The following copies to `_site/assets`
   eleventyConfig.addPassthroughCopy("site/assets");
   eleventyConfig.addPassthroughCopy("site/uploads");
-  eleventyConfig.addPassthroughCopy("admin");
 
   // Transforms
   eleventyConfig.addTransform("htmlmin", htmlmin);
